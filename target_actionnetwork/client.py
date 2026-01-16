aa4193fd7c5e46fc68705a883eadddc5135fdcbf
paulmdev <pdmakarev@gmail.com>
2026-01-16 10:19:13 -0500

    Added target monitoring error handling and fixed "number" field name.

--- target_actionnetwork/client.py
+++ target_actionnetwork/client.py
@@ -4,24 +4,29 @@ from __future__ import annotations
 
 import backoff
 import requests
-from singer_sdk.exceptions import RetriableAPIError
 
-from target_hotglue.client import HotglueSink
+from hotglue_singer_sdk.plugin_base import PluginBase
+from hotglue_singer_sdk.target_sdk.client import HotglueSink
+from hotglue_etl_exceptions import InvalidCredentialsError, InvalidPayloadError
+from hotglue_singer_sdk.exceptions import FatalAPIError, RetriableAPIError
+import json
 
 from target_actionnetwork.auth import ActionNetworkAuthenticator
 
+class RetriableInvalidPayloadError(InvalidPayloadError, RetriableAPIError):
+    pass
 
 class ActionNetworkSink(HotglueSink):
     """ActionNetwork target sink class."""
 
     @backoff.on_exception(
-        backoff.expo,
+        backoff.expo,   
         (RetriableAPIError, requests.exceptions.ReadTimeout),
-        max_tries=8,
+        max_tries=7,
         factor=2,
     )
     def _request(
-        self, http_method: str, endpoint: str, params: dict=None, request_data: dict = None, headers: dict = None
+        self, http_method: str, endpoint: str, params: dict=None, request_data: dict = None, headers: dict = None, verify = True
     ) -> requests.PreparedRequest:
         """Prepare a request object."""
         url = self.url(endpoint)
@@ -37,6 +42,27 @@ class ActionNetworkSink(HotglueSink):
         )
         self.validate_response(response)
         return response
+
+
+    def validate_response(self, response: requests.Response) -> None:
+        """Validate HTTP response."""
+        msg = response.text
+
+        # 403 was observed, going to put 401 here so we don't accidentally expose the key
+        if response.status_code == 403 or response.status_code == 401:
+            # Response includes the API key, which should NOT be exposed. DO NOT REMOVE.
+            if "API Key" in msg:
+                raise InvalidCredentialsError("API Key invalid or not present.")
+            raise InvalidCredentialsError(msg)
+
+        if response.status_code == 400:
+            raise InvalidPayloadError(msg)
+
+        # Either bad request or internal server error, unknown which
+        if response.status_code == 500:
+            raise RetriableInvalidPayloadError(msg)
+
+        super().validate_response(response)
     
     @property
     def base_url(self) -> str:
